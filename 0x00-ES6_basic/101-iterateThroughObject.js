b821ed58d0556480333cf8373eb9ed8fc56c6477
L eye <leyume@users.noreply.github.com>
2023-06-26 21:32:22 +0100

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,3 @@
-/* eslint-disable */
 export default function iterateThroughObject(reportWithIterator) {
-   let itemstr = '', i = 0;
-   for (const item of reportWithIterator) {
-        if (reportWithIterator.length - 1 == i) {
-            itemstr += item
-        }
-        else {
-            itemstr += item + ' | ';
-        }
-        i++;
-   }
-   return itemstr;
+  return [...reportWithIterator].join(' | ');
 }
