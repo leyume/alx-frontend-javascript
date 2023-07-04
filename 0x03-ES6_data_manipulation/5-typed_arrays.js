d539496f8d537f4aa782916937e7b511e556eb3b
L eye <leyume@users.noreply.github.com>
2023-07-04 20:59:51 +0100

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,10 +2,12 @@
  * Creates a buffer array with a given position set to a given value.
  */
 export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer, 0, length);
+
   if (position >= length) {
-    throw new Error("Position outside range");
+    throw Error('Position outside range');
   }
-  const buf = new DataView(new ArrayBuffer(length), 0, length);
-  buf.setInt8(position, value);
-  return buf;
+  view.setInt8(position, value);
+  return view;
 }
